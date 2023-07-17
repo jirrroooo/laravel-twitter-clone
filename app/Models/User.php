624955585668f9a573493558b1e6cdee8ed4f453
jiro.octavo <jiro.octavo@gmail.com>
2023-07-18 00:23:29 +0800

    chore: add follows logic

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,4 +37,8 @@ class User extends Authenticatable
     public function tweets(){
         return $this->hasMany(Tweet::class);
     }
+
+    public function follows(){
+        return $this->belongsToMany(User::class, 'follows', 'user_id', 'following_user_id');
+    }
 }
